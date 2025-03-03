d4ab8bb3e8dabb1e0ef0a05f773863d1c5018966
Claus Guttesen <kometen@gmail.com>
2025-03-03 17:10:33 +0100

    Add passwordless to about page

--- src/about_page.rs
+++ src/about_page.rs
@@ -10,7 +10,14 @@ struct AboutPage;
 #[async_trait]
 impl AboutTemplate for AboutPage {
     async fn render(&self) -> Template {
-        Template::render("about", context! {})
+        let passwordless_api_key =
+            &std::env::var("PASSWORDLESS_API_KEY").expect("PASSWORDLESS_API_KEY must be set.");
+        let passwordless_api_url =
+            &std::env::var("PASSWORDLESS_API_URL").expect("PASSWORDLESS_API_URL must be set.");
+        Template::render(
+            "about",
+            context! { passwordless_api_url: passwordless_api_url, passwordless_api_key: passwordless_api_key  },
+        )
     }
 }
 
